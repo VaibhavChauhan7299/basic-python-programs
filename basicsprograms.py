8438bce75393cbef18afacbd0c1d571a200d9bcb
MeghVyas3132 <you@example.com>
2025-12-30 01:07:54 +0530

    Initial commit: Learn basic Python programs

---
+++ basicsprograms.py
@@ -0,0 +1,183 @@
+#print("Hello Vaibhav")
+#name="Vaibhav Chauhan💕"
+#age=22
+#price=25.99
+#print("my name is :",name)
+#print("my age is :",age)
+#print("the price is :",price)
+
+
+#print(type(name))
+#print(type(age))
+#print(type(price))
+
+
+#a=500
+#b=55
+#sum= a + b
+#print(sum)
+#diff = a - b
+#print(diff)
+
+
+#A,B=2,3
+#txt="@"
+#print(2*txt*3)
+
+
+#A,B="2",3
+#txt="@"
+#print((A+txt)*B)
+
+
+#A,B=10,20
+#C=4
+#print(A+B*C)
+
+
+#a,b=10,5.0
+#c=a*b
+#print(c)
+
+#a,b=1,2
+#c=a/b
+#print(c)
+
+#a,b=1.5,3
+#c=a//b
+#print(c, a/b)
+
+
+#A,B=-5,2
+#C=A%B
+#print(C)
+
+
+#A,b=5,2
+#C=A%B
+#print(C)
+
+
+#A,B=5,-2
+#C=A%B
+#print(C)
+
+#name=input("name:")
+#age=int(input("age:"))
+#price=float(input("price:"))
+
+#print("my name is :",name,",and I am",age,"years old","I Like this price",price)
+
+#conditional statements
+
+# light= input("light color :")
+
+# if (light== "red"):
+#     print("stop")
+# elif (light== "yellow"):
+#     print("get ready")
+# elif(light== "green"):
+#     print("go")
+# else:
+#     print("LIGHT IS BROKEN")
+
+# second example of conditional statements
+#A = int(input("A :"))
+#G = input("M/F/CHILD3 :")
+
+# if((A==1 or A==2) and G=="M"):
+#     print("fee is 100")
+# elif((A==3 or A==4) and G=="F"):
+#     print("fee is 200")
+# elif((A==5 and G=="CHILD")):
+#     print("fee is 300")
+# else:
+#     print("No Fee")
+
+#single line if-else (ternary operator)
+# format= <var>=<val1> if <condition> else <val2>
+#food=input("food :")
+#eat="yes" if food=="cake" else "no"
+#print(eat)
+
+#format= <stt1> if <condition> else <stt2> 
+# food=input("food :")
+# print("sweet") if food == "cake" or food == "chocolate" else print("not sweet")
+
+#clever if/ternary operator
+# age=int(input("age :"))
+# vote=("yes", "no") [age<18]
+# print(vote)
+
+# sal= float(input("salary :"))
+# tax= sal*(0.1, 0.2) [sal>50000]
+# print(tax)
+
+# p = float(input("p :"))
+# r = float(input("r :"))
+# t = float(input("t :"))
+# si = (p*r*t)/100
+# print("simple interest is :",si)
+
+
+#arithmetic operators
+# a=10
+# b=3
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** b)   
+
+#relational operators
+# a=50
+# b=20
+
+# print(a == b) #False
+# print(a != b) #True
+# print(a >= b) #True
+# print(a <= b) #False
+# print(a > b)  #True
+# print(a < b)  #False
+
+#assignment operators
+#num=10
+# num = num + 5 #10+5=15
+# print("num:",num)
+#num += 5
+#num -= 5
+# num *= 5
+# num /= 5
+# num %= 5
+# num **= 5
+#print("num:",num)
+
+#logical operators
+
+# a=50
+# b=30
+# # print(not (a > b)) #false
+
+# val1 = True
+# val2 = False
+# print("AND operator:", val1 and val2) 
+
+# print("OR operator:", val1 or val2)
+
+# print("OR operator:", a==b or a>b)
+
+#Type Conversion , Casting
+
+# a=int("20")
+# b=5.5
+# #sum= a+b #25.5 (float)
+# #print(sum) 
+
+# print("sum ", a + b)
+
+# a = "yashvi"
+
+# print(type(a))

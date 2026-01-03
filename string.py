bff4eb80a3d3890d0c5e9ce7db02fe95f1b75c31
VaibhavChauhan7299 <vaibhavchauhan7299@gmail.com>
2026-01-03 20:43:48 +0530

    added more basic code

--- string.py
+++ string.py
@@ -2,6 +2,46 @@
 
 #string is data type that stores a sequence of characters.
 
-str1 = "Hello, World!"
-str2 = 'Python is fun.'
-str3 = """This is a string"""
+# str1 = "Hello, World!"
+# str2 = 'Python is fun.'
+# str3 = """This is a string"""
+
+# str1 = "This is a string..\t with a tab space."
+# print(str1)
+
+# str1 = "hello"
+# len1 =len(str1)
+# print(len1)
+
+# str2 = "helluu"
+# len2 =len(str2)
+# print(len2)
+
+# final_str = str1+ " " + str2
+# print(str1+str2)
+# print(final_str)
+# print(len(final_str))
+
+#indexing in string
+
+# str = "Vaibhav Chauhan"
+# ch =str[0]
+# print(ch)
+
+#slicing in string
+
+# str = "Vaibhav Chauhan"
+# print(str[0:7])  # Vaibhav
+# print(str[8:15]) # Chauhan
+# print(str[:7])   # Vaibhav
+# print(str[6:])   # v Chauhan
+# print(str[:])    # Vaibhav Chauhan
+
+#negative indexing in string
+
+# str = "Vaibhav Chauhan"
+# print(str[-1])    # n
+# print(str[-7:])   # Chauhan
+# print(str[:-7])   # Vaibhav
+# print(str[-7:-1]) # Chauha
+# print(str[-15:]) # Vaibhav Chauhan

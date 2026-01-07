6721e61f1f459afdbf7578a03f3b19c0a419d2bf
VaibhavChauhan7299 <vaibhavchauhan7299@gmail.com>
2026-01-07 18:18:41 +0530

    lists

--- lists.py
+++ lists.py
@@ -1,3 +1,41 @@
 #lists
 # a built in data type that stores set of values , it can store elements of different types(integer , float , string , etc..
 
+# marks = [94.4, 87.5, 95, 55, 99]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+
+# student = ["John", 21, "A+", True]
+# print(student)
+# student[0] = "Yashu"
+# print(student)
+
+#list slicing
+#list_name [starting_index : ending_index] #ending idx is not included
+
+# marks = [85, 55, 90, 78, 88, 92, 67, 79]
+# print(marks[1:4])
+# print(marks[2:5])
+# print(marks[0:3])
+# print(marks[-4:-1])
+
+#list methods
+
+list = [5, 1, 5, 15, 20, 25]
+
+# list.append(30) #adds one element at the end
+# print(list)
+# print(list.sort())# sorts in ascending order
+# print(list)
+# list.sort(reverse=True) #sorts in descending order
+# print(list)
+# list.reverse() #reverses the list
+# print(list)
+# list.insert(2, 10) #insert element at index
+# print(list)
+#list.remove(5) #removes first occurrence of elemenet
+#print(list)
+# list.pop(2) #removes element at idx
+# print(list)

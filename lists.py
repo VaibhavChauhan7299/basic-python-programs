c4a94cf8783576ed605f61a2057364dda3081cd2
VaibhavChauhan7299 <vaibhavchauhan7299@gmail.com>
2026-01-07 20:06:56 +0530

    some modified

--- lists.py
+++ lists.py
@@ -39,3 +39,31 @@ list = [5, 1, 5, 15, 20, 25]
 #print(list)
 # list.pop(2) #removes element at idx
 # print(list)
+
+#write a program to ask the user to enter names of their 3 favorite movies & them in a list.
+
+# movies = []
+# mov1 = input("enter 1st movie:")
+# mov2 = input("enter 2nd movie:")
+# mov3 = input("enter 3rd movie:")
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+# print(movies)
+
+# 2 write a program to check if a list contains a palideome of elemnts.(hint:use copy()method)
+
+list1 = [1, 2, 1]
+list2 = [1, 2, 3]
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if(copy_list1 == list1):
+    print("palindrome")
+else:
+    print("NOT palindrome")
+    
+
+    
---
+++ tuples.py
@@ -0,0 +1,25 @@
+# A built-data type that lets us create immutable sequences of values.
+
+#tup = (2, 4, 5, 7,)
+# print(tup[0])
+# print(tup[1])
+#print(type(tup))
+
+#tuple methods
+
+#tup = (1, 2, 3, 4, 5, 6, 7, 8, 9, 9,)
+#print(tup.index(3)) #returns index of first occurrence
+#print(tup.count(9)) #counts total occurrences
+
+#write to count the number of students with the "A" grade in the following tuple.
+
+#grade = ("c", "D", "A", "A", "B", "B", "A")
+#print(grade.count("A"))
+
+
+#store the above value in a list & sort them from "A" to "D".
+
+# list = ["C", "D", "A", "A", "B", "B", "A"]
+# list.sort() 
+# print(list)
+

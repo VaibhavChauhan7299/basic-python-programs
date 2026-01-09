965a5ce05a826fa5980169367329b3a2a81cf166
VaibhavChauhan7299 <vaibhavchauhan7299@gmail.com>
2026-01-09 10:41:53 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+#Dictionary
+#Dictionaries are used to store data values in key:value pairs they are unordered, mutable(changeable)& don't allow duplicate keys
+
+info = {
+    "key" : "value",
+    "name" : "vaibhav",
+    "learning" : "coding",
+    "age" : 35,
+    "is_adult" : True,
+    "marks" : 94.4,
+    "name" : "VAIBHAVCHAUHAN",
+    "subjects" : ["python", "c", "Java"],
+    "topics" : ("dict", "set"),
+}
+
+# print(info)
+# print(type(info))
+# print(info["subjects"])
+
+info["name"] = "yashvi" #overwrite
+info["surname"] = "sidapara"
+print(info)

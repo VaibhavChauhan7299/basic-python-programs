788cf06fe76cb702b53b4fef10b1f9c7c93bddbe
VaibhavChauhan7299 <vaibhavchauhan7299@gmail.com>
2026-01-09 19:28:03 +0530

    modified

--- dictionary.py
+++ dictionary.py
@@ -1,22 +1,34 @@
 #Dictionary
 #Dictionaries are used to store data values in key:value pairs they are unordered, mutable(changeable)& don't allow duplicate keys
 
-info = {
-    "key" : "value",
-    "name" : "vaibhav",
-    "learning" : "coding",
-    "age" : 35,
-    "is_adult" : True,
-    "marks" : 94.4,
-    "name" : "VAIBHAVCHAUHAN",
-    "subjects" : ["python", "c", "Java"],
-    "topics" : ("dict", "set"),
-}
+# info = {
+#     "key" : "value",
+#     "name" : "vaibhav",
+#     "learning" : "coding",
+#     "age" : 35,
+#     "is_adult" : True,
+#     "marks" : 94.4,
+#     "name" : "VAIBHAVCHAUHAN",
+#     "subjects" : ["python", "c", "Java"],
+#     "topics" : ("dict", "set"),
+# }
 
 # print(info)
 # print(type(info))
 # print(info["subjects"])
 
-info["name"] = "yashvi" #overwrite
-info["surname"] = "sidapara"
-print(info)
+# info["name"] = "yashvi" #overwrite
+# info["surname"] = "sidapara"
+# print(info)
+
+#nested dictionary
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+print(student["subjects"])

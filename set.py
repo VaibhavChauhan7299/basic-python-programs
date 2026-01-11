c8f579eea31a1acfc2cf8862c32d7823464e98d2
VaibhavChauhan7299 <vaibhavchauhan7299@gmail.com>
2026-01-11 19:25:10 +0530

    set in python

---
+++ set.py
@@ -0,0 +1,25 @@
+#set
+#set is the collection of the unorderd items.
+#Each element in the set must be unique & immutable.
+
+# collection = {1,2,2,3,4, "hello", "world"}
+
+# print(collection)
+# print(type(collection))
+# print(len(collection))
+
+#collection = set()
+#print(collection)
+
+#set Methods
+# set.add(el)#adds an element
+# set.remove(el)#removes the elem an
+# set.clear()#emties the set
+# set.pop()#removes a random value
+
+collection = set()
+collection.add(1)
+collection.add(2)
+
+collection.remove(1)
+print(collection)

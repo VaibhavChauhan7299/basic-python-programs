e6edbfed090612d66f36012520f4e7a6dd7b8ef2
VaibhavChauhan7299 <vaibhavchauhan7299@gmail.com>
2026-01-16 19:28:27 +0530

    Loops

---
+++ loops.py
@@ -0,0 +1,27 @@
+#loops
+#loops are used to repeat instructions.
+#types of loops:
+#1. for loop
+#2. while loop
+
+#while Loops
+
+count = 1
+while count<=5:
+    print("Hello Vaibhav")
+    count = count + 1
+print(count)
+
+i = 1
+while i<=100000:
+    print("YASHVIVAIBHAV", i)
+    i = i + 1
+print(i)
+#not: make sure to update the condition variable inside the loop to avoid infinite loop.
+
+#print numbers from 1 to 5
+i = 1
+while i<=5:
+    print(i)
+    i = i + 1
+print("Done")
--- set.py
+++ set.py
@@ -46,9 +46,24 @@
 
 #you are given a list of subjects for students. Assume one classroom is required for 1 subject. How many classrooms are needed by all students.
 
-subjects = {
-    "python", "java", "c++", "python", "java", "javascript",
-    "python", "c++","c"
-}
+# subjects = {
+#     "python", "java", "c++", "python", "java", "javascript",
+#     "python", "c++","c"
+# }
 
-print(len(subjects))
+# print(len(subjects))
+
+#Figure out a way to store 9 & 9.0 as separate values in the set.
+#(You can take help of built-in data types)
+
+#solutuion-1
+values = {9, "9.0"}
+print(values)
+
+#solution-2
+
+# values = {
+#     ("float", 9.0),
+#     ("int", 9)
+# }
+# print(values)

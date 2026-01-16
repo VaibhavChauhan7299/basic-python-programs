04f8ecf90100497eea16d7bf2c9cb4074a763917
VaibhavChauhan7299 <vaibhavchauhan7299@gmail.com>
2026-01-16 20:46:36 +0530

    modified

--- loops.py
+++ loops.py
@@ -6,22 +6,77 @@
 
 #while Loops
 
-count = 1
-while count<=5:
-    print("Hello Vaibhav")
-    count = count + 1
-print(count)
+# count = 1
+# while count<=5:
+#     print("Hello Vaibhav")
+#     count = count + 1
+# print(count)
 
-i = 1
-while i<=100000:
-    print("YASHVIVAIBHAV", i)
-    i = i + 1
-print(i)
+# i = 1
+# while i<=100000:
+#     print("YASHVIVAIBHAV", i)
+#     i = i + 1
+# print(i)
 #not: make sure to update the condition variable inside the loop to avoid infinite loop.
 
 #print numbers from 1 to 5
-i = 1
-while i<=5:
-    print(i)
-    i = i + 1
-print("Done")
+# i = 1
+# while i<=5:
+#     print(i)
+#     i = i + 1
+# print("Done")
+
+#print muliplication table of 35 using while loop
+
+# i = 1
+# while i<=10:
+#     print(35,"x",i,"=",35*i)
+#     i = i + 1
+
+#print number from 5 to 1
+
+# i = 5
+# while i>=1:
+#     print(i)
+#     i = i - 1
+# print("Done")
+
+#print number from 1 to 100
+
+# i = 1 
+# while i<=100:
+#     print(i)
+#     i = i + 1
+
+#print number 100 to 1
+
+# i = 100
+# while i>=1:
+#     print(i)
+#     i = i - 1
+
+#print the multiplication table of a number n.
+
+# i = 1
+# n = int(input("Enter the number :"))
+# while i<=10:
+#     print(n, "x", i, "=", n*i)
+#     i+=1
+
+#print the elements of the following list using a loop:
+    
+# values = [1,4,9,16,25,36,49,64,81,100]
+# index = 0
+# while index<len(values):
+#     print(values[index])
+#     index += 1
+
+#example
+
+# heros = ["Ironman", "Thor", "Hulk", "Captain America", "Black Widow"]
+# i = 0
+# while i<len(heros):
+#     print(heros[i])
+#     i += 1
+
+#search for a number x in this tuple using loop:

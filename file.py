c97e4af7b3b4d9517c6ef5ede21782194cb9f906
VaibhavChauhan7299 <vaibhavchauhan7299@gmail.com>
2026-01-26 05:27:08 +0530

    I/O files

---
+++ file.py
@@ -0,0 +1,46 @@
+#file I/O in python
+
+#python can be used to perform operations on a file.(read & write data)
+
+#types of files:
+#1. Text files (.txt, .csv, .html, .xml, .json, .py, .java, .c, .cpp, etc.)
+#2. Binary files (.bin, .dat, .exe, .jpg, .png, .mp3, .mp4, etc.)
+
+#Open, Read & Close File
+
+#we have to open a file before reading or writing.
+
+#f = open("file_name", "mode")
+
+# f = open("lists.py" , "r") #opens the file in read mode
+# data = f.read(5) #reads first 5 characters from the file
+# print(data)
+# print(type(data))
+# f.close() #closes the file
+
+#modes:
+#charcter                       meaning
+# "r" - open for reading (default)
+# "w" - open for writing, truncating the file first
+# "x" - create a new file and open it for writing
+# "a" - open for writing, appending to the end of the file if exists
+# "b" - binary mode
+# "t" - text mode (default)
+# "+" - open a file for updating (reading and writing)
+
+#reading a file:
+
+# data = f.read() #reads the entire file
+
+# line = f.readline() #reads one line from the file
+
+
+f = open("loops.py", "r")
+
+line1 = f.readline()
+print(line1)
+
+line2 = f.readline()
+print(line2)
+
+f.close()

90f48083f151151db2721b2204d9ada7eef53b14
VaibhavChauhan7299 <vaibhavchauhan7299@gmail.com>
2026-06-23 17:58:43 +0530

    oops

--- OOP.py
+++ OOP.py
@@ -108,16 +108,49 @@
 
 #ex:
 
-class Car:
-    def __init__(self):
-        self.acc = False
-        self.brk = False
-        self.clutch = False
+# class Car:
+#     def __init__(self):
+#         self.acc = False
+#         self.brk = False
+#         self.clutch = False
 
-    def start(self):
-        self.clutch = True    
-        self.acc = True
-        print("car started..")
+#     def start(self):
+#         self.clutch = True    
+#         self.acc = True
+#         print("car started..")
 
-car1 = Car()
-car1.start()
+# car1 = Car()
+# car1.start()
+
+
+#2 Encapsulation: wrapping data and functions into a single unit (object).
+
+#question: Create Account class with 2 attributes - balance & account no. .Create a method to deposit and withdraw money from the account.
+
+class Account:
+    def __init__ (self, bal, acc):
+        self.balance = bal
+        self.account_no = acc
+
+    def debit(self, amount):
+        self.balance -= amount
+        print("Rs. ", amount, "was debited")
+        print("total balance =", self.get_balance())
+
+
+
+    def credit(self, amount):
+        self.balance += amount
+        print("Rs. ", amount, "was credited")
+        print("total balance =", self.get_balance())
+
+
+    def get_balance(self):
+        return self.balance        
+    
+acc1 = Account (10000, 12345) 
+# print(acc1.balance)
+# print(acc1.account_no)   
+acc1.debit(5000)
+acc1.credit(2000)
+acc1.credit(55000)

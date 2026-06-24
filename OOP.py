8f6157e86dc81f3626b52ff84e1f7e15b48d6658
VaibhavChauhan7299 <vaibhavchauhan7299@gmail.com>
2026-06-24 19:06:42 +0530

    some changes

--- OOP.py
+++ OOP.py
@@ -175,15 +175,15 @@
 #Conceptual Implementations in python : private attributes & methods are meant to be used only within the class and are not accessible from outside the class.
 # ex
 
-class Account:
-    def __init__(self, acc_no, acc_pass):
-        self.acc_no = acc_no
-        self.__acc_pass = acc_pass
+# class Account:
+#     def __init__(self, acc_no, acc_pass):
+#         self.acc_no = acc_no
+#         self.__acc_pass = acc_pass
 
-    def reset_pass(self):
-        print(self.__acc_pass)    
+#     def reset_pass(self):
+#         print(self.__acc_pass)    
 
-acc1 = Account("12345", "abc@123")
+# acc1 = Account("12345", "abc@123")
 
-print(acc1.acc_no)
-print(acc1.reset_pass())
+# print(acc1.acc_no)
+# print(acc1.reset_pass())

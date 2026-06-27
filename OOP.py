97004765d69e975ef912e510dc8ae0695f27183e
VaibhavChauhan7299 <vaibhavchauhan7299@gmail.com>
2026-06-27 18:06:28 +0530

    compeleted OOPS2

--- OOP.py
+++ OOP.py
@@ -356,19 +356,19 @@
 # Define an Area() method of the class which calculate the area of the circle.
 # Define a pwrimeter() method of the class which allows you to calculate the primeter of the circle.
 
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
+# class Circle:
+#     def __init__(self, radius):
+#         self.radius = radius
 
-    def area(self):
-        return (22/7) * self.radius ** 2
+#     def area(self):
+#         return (22/7) * self.radius ** 2
 
-    def perimeter(self):
-        return 2 * (22/7) * self.radius
+#     def perimeter(self):
+#         return 2 * (22/7) * self.radius
 
-c1 = Circle(21)
-print(c1.area())
-print(c1.perimeter())        
+# c1 = Circle(21)
+# print(c1.area())
+# print(c1.perimeter())        
 
 #Qs 2
 
@@ -398,4 +398,19 @@ print(c1.perimeter())
 # engg1 = Engineer("Elon Musk", 40)
 # engg1.showDetails()
 
-#
+#Qs: Create a class called order which stores item & its price.
+#   Use Dunder function __gt__()to convey that :
+#   order1 > order2 if price of order1 > price of order2
+
+class Order:
+    def __init__(self, item, price):
+        self.item = item
+        self.price = price
+
+    def __gt__(self, odr2):
+        return self.price > odr2.price
+
+odr1 = Order("chips", 20)
+odr2 = Order("tea", 15) 
+
+print( odr1 > odr2) #true

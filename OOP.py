eb32352b8262d2dcb29116f89873ce1fb2e71d0b
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-06-28 22:57:20 +0530

    some changes

--- OOP.py
+++ OOP.py
@@ -402,15 +402,15 @@
 #   Use Dunder function __gt__()to convey that :
 #   order1 > order2 if price of order1 > price of order2
 
-class Order:
-    def __init__(self, item, price):
-        self.item = item
-        self.price = price
+# class Order:
+#     def __init__(self, item, price):
+#         self.item = item
+#         self.price = price
 
-    def __gt__(self, odr2):
-        return self.price > odr2.price
+#     def __gt__(self, odr2):
+#         return self.price > odr2.price
 
-odr1 = Order("chips", 20)
-odr2 = Order("tea", 15) 
+# odr1 = Order("chips", 20)
+# odr2 = Order("tea", 15) 
 
-print( odr1 > odr2) #true
+# print( odr1 > odr2) #true

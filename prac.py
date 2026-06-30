4b26494f4c1356c7632951891af7f9861c84a4f1
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-06-30 23:50:32 +0530

    practice program

--- prac.py
+++ prac.py
@@ -37,3 +37,17 @@
 #         right -= 1
 #     return True
 
+
+# class Circle:
+#     def __init__(self, radius):
+#         self.radius = radius
+
+#     def area(self):
+#         return (22/7) * self.radius ** 2
+
+#     def perimeter(self):
+#         return 2 * (22/7) * self.radius
+
+# c1 = Circle(21)
+# print(c1.area())
+# print(c1.perimeter())

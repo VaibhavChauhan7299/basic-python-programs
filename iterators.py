d108217779882bea797a8d9f5bb9bade96c851c6
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-07-06 16:48:44 +0530

    iterators

--- iterators.py
+++ iterators.py
@@ -1,2 +1,21 @@
 #iterators: iterators are objects that allow you to traverse through a collection of elements, such as lists, tuples, or dictionaries. In Python, an iterator is an object that implements the iterator protocol, which consists of two methods: __iter__() and __next__().
-#iterators are advanced python concepts that allow for efficient looping and memory management. Iterators provide a way to access elements of collection sequentially without exposing the underlying sturcture.
+#iterators are advanced python concepts that allow for efficient looping and memory management. Iterators provide a way to access elements of collection sequentially without exposing the underlying structure.
+
+# my_list = [1, 2, 3, 4, 5, 6]
+# for i in my_list:
+#     print(i)
+
+# print(type(my_list))
+
+
+#creating iterator using iterator() function:
+
+# iterator = iter(my_list)
+# print(type(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))  # This will raise StopIteration error since there are no more elements to iterate over.

54af2b1bfff860c676eadf672e286e6b12d6bef7
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-07-07 21:51:14 +0530

    patterns program

---
+++ patterns.py
@@ -0,0 +1,44 @@
+#programs of patterns in python:
+#method 1:
+# print("**************")
+# print("*************")
+# print("************")
+# print("***********")
+# print("**********")
+# print("*********")
+# print("********")
+# print("*******")
+# print("******")
+# print("*****")
+# print("****")
+# print("***")
+# print("**")
+# print("*")
+
+#method 2:
+
+# i = 10
+# print('*' * i)
+# i = 9
+# print('*' * i)
+# i = 8
+# print('*' * i)
+# i = 7
+# print('*' * i)
+# i = 6
+# print('*' * i)
+# i = 5
+# print('*' * i)
+# i = 4
+# i = 3
+# print('*' * i)
+# i = 2
+# print('*' * i)
+# i = 1
+# print('*' * i)
+
+#method 3:
+
+# for i in range(10, 0, -1):
+#     print('*' * i)
+

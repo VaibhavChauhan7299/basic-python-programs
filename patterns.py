06cef67a9b17c480d6b90d959f8750cd0605cd69
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-07-10 08:04:14 +0530

    patterns program

--- patterns.py
+++ patterns.py
@@ -61,3 +61,15 @@
 #         print(j + 1, end='')
 #     print()
     
+#heart pattern:
+
+# for i in range(6):
+#     for j in range(7):
+#         if (i == 0 and j % 3 != 0) or \
+#         (i == 1 and j % 3 == 0) or \
+#         (i - j == 2) or \
+#         (i + j == 8):
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print()

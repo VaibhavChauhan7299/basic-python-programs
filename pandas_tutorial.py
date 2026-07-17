7fe260b6ee2acf01c7675796c638099aa9c1d2c9
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-07-17 14:54:57 +0530

    Pandas-tutorial

--- pandas_tutorial.py
+++ pandas_tutorial.py
@@ -1,8 +1,18 @@
 import pandas as pd
 
-x = [1, 2, 3, 4, 5]
+# x = [1, 2, 3, 4, 5] #list 
 
-s = pd.Series(x)
+# s = pd.Series(x, index = ['a', 'b', 'c', 'd', 'e'], dtype = "float", name = "My First Series")
 
-print(s)
-print(type(s))
+# print(s)
+
+# print(type(s))
+
+# print(s['e'])#output: 5
+
+#dictionary
+
+# dict = {"name":["python", "c", "c++", "java"], "por":[12, 13, 14, 15], "rank": [1, 4, 3, 2]}
+# var1 = pd.Series(dict)
+
+# print(var1)

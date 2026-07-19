182f23fbd090af2f5c814cfe14a4dd663b46267f
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-07-19 19:58:02 +0530

    Pandas-Tutorial

--- pandas_tutorial.py
+++ pandas_tutorial.py
@@ -19,16 +19,16 @@ import pandas as pd
 
 #####
 
-#s = pd.Series(12, index = [1,2,3,4,5,6,7,8,9,10])
-# print(s)
-# print(type(s))
+s = pd.Series(12, index = [1,2,3,4,5,6,7,8,9,10])
+print(s)
+print(type(s))
 
-# s1 = pd.Series(12, index = [1,2,3,4,5,6,7,8,9,10])
-# s2 = pd.Series(12, index = [1,2,3,4,5])
+s1 = pd.Series(12, index = [1,2,3,4,5,6,7,8,9,10])
+s2 = pd.Series(12, index = [1,2,3,4,5])
 
-# print(s1+s2) #output: 1    24.0
-# print(s1-s2) #output: 1     0.0
-# print(s1*s2) #output: 1    144.0
-# print(s1/s2) #output: 1    1.0
-# print(s1)
-# print(s2)
+print(s1+s2) #output: 1    24.0
+print(s1-s2) #output: 1     0.0
+print(s1*s2) #output: 1    144.0
+print(s1/s2) #output: 1    1.0
+print(s1)
+print(s2)

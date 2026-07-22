872f4c2b33577ad46f013b7887d4f4f9c411e1dc
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-07-22 23:55:18 +0530

    some changes

--- pandas_tutorial.py
+++ pandas_tutorial.py
@@ -50,8 +50,8 @@ import pandas as pd
 
 #list of list
 
-list_1 = [[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15]]
+# list_1 = [[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15]]
 
-var2 = pd.DataFrame(list_1)
-print(var2)
-print(type(var2))
+# var2 = pd.DataFrame(list_1)
+# print(var2)
+# print(type(var2))

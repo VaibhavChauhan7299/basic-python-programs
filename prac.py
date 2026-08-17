fe190490606c32b0f972fd19a598146bf58a864c
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-08-17 23:47:44 +0530

    Practice program

--- prac.py
+++ prac.py
@@ -428,3 +428,12 @@
 #     print("Both are equal")
 # else:
 #     print("Both are not equal")    
+
+##write a program to repeated Alphabet Triangle.
+
+rows = int(input("Enter the number of rows: "))
+
+for i in range(1, rows + 1):
+    for j in range(i):
+        print(chr(64 + i), end=" ")
+    print()

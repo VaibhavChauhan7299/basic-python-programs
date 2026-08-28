42bab44b7e433a4196623f9e9b5e019d7ca58f7b
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-08-28 20:00:00 +0530

    Update Python practice

--- prac.py
+++ prac.py
@@ -625,11 +625,36 @@
 #         else:
 #             print("You can't move there. Try again.")
 
-# Practice: Check even or odd
+## Practice: Check even or odd
 
-number = 10
+# number = 10
 
-if number % 2 == 0:
-    print("Even number")
-else:
-    print("Odd number")
+# if number % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+##write a hard program to find the longest palindromic substring in a given string. A palindromic substring is a sequence of characters that reads the same backward as forward.
+
+def longest_palindromic_substring(s):
+    if len(s) < 1:
+        return ""
+
+    start, end = 0, 0
+
+    for i in range(len(s)):
+        len1 = expand_around_center(s, i, i)      # Odd length palindromes
+        len2 = expand_around_center(s, i, i + 1)  # Even length palindromes
+        max_len = max(len1, len2)
+
+        if max_len > (end - start):
+            start = i - (max_len - 1) // 2
+            end = i + max_len // 2
+
+    return s[start:end + 1]
+
+def expand_around_center(s, left, right):
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+    return right - left - 1

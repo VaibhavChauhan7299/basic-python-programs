efc1c93cbbc972e206fe2ceb0ac75b09fcd7d431
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-09-03 10:00:00 +0530

    Practice Program

--- prac.py
+++ prac.py
@@ -727,3 +727,14 @@
 
 
 # print(max_occurring_char(text))  # Output: "a"
+
+##from a list of numbers, move all zeros to the end of the list.
+
+list = [0, 1, 2, 0, 3, 4, 0, 5]
+
+for item in list:
+    if item == 0:
+        list.remove(item)
+        list.append(item)
+
+print(list)  # Output: [1, 2, 3, 4, 5, 0, 0, 0]

99350fc72626a7f1d4920696457f9999406e12ca
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-09-03 10:40:00 +0530

    Practice Program in some changes

--- prac.py
+++ prac.py
@@ -730,11 +730,11 @@
 
 ##from a list of numbers, move all zeros to the end of the list.
 
-list = [0, 1, 2, 0, 3, 4, 0, 5]
+# list = [0, 1, 2, 0, 3, 4, 0, 5]
 
-for item in list:
-    if item == 0:
-        list.remove(item)
-        list.append(item)
+# for item in list:
+#     if item == 0:
+#         list.remove(item)
+#         list.append(item)
 
-print(list)  # Output: [1, 2, 3, 4, 5, 0, 0, 0]
+# print(list)  # Output: [1, 2, 3, 4, 5, 0, 0, 0]

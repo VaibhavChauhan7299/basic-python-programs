65c269c719fbce3035b5026e57a05ad8517e892b
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-09-06 18:09:00 +0530

    Practice Program

--- prac.py
+++ prac.py
@@ -782,11 +782,11 @@
 
 ###write a function to find the longest word in a sentence using max().
 
-def find_longest_word(sentence):
+# def find_longest_word(sentence):
 
-    words = sentence.split()
-    longest_word = max(words, key=len)
-    return longest_word
+#     words = sentence.split()
+#     longest_word = max(words, key=len)
+#     return longest_word
 
-print(find_longest_word("The quick brown fox jumps over the lazy dog jermanshepherd")) 
+# print(find_longest_word("The quick brown fox jumps over the lazy dog jermanshepherd")) 
 

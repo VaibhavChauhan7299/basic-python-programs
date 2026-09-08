736182b0cdb2953d2e1fffab7a44f17e032b8949
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-09-08 21:05:00 +0530

    Practice Program

--- prac.py
+++ prac.py
@@ -792,15 +792,15 @@
 
 ##write a bubble sort program to sort a list of numbers in ascending order.
 
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr
+# def bubble_sort(arr):
+#     n = len(arr)
+#     for i in range(n):
+#         for j in range(0, n - i - 1):
+#             if arr[j] > arr[j + 1]:
+#                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+#     return arr
 
 # Example usage:
-numbers = [64, 34, 25, 12, 22, 11, 90]
-sorted_numbers = bubble_sort(numbers)
-print("Sorted list:", sorted_numbers)
+# numbers = [64, 34, 25, 12, 22, 11, 90]
+# sorted_numbers = bubble_sort(numbers)
+# print("Sorted list:", sorted_numbers)

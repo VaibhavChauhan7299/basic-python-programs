82f1f28b765f3114562c5b498e894287f91fc049
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-09-10 20:10:00 +0530

    Practice Program

--- prac.py
+++ prac.py
@@ -864,3 +864,21 @@
 # sorted_numbers = merge_sort(numbers)
 # print("Sorted list:", sorted_numbers)  
 
+##write a program insertion sort to sort a list of numbers in ascending order.
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+    return arr
+
+# Example usage:
+numbers = [12, 11, 13, 5, 6]
+sorted_numbers = insertion_sort(numbers)
+print("Sorted list:", sorted_numbers)  # Output: [5, 6, 11, 12, 13]

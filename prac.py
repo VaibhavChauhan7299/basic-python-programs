a43cbf3ef4dbcc7a47f763122cf732dca20c08d0
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-09-10 20:10:00 +0530

    Practice Program

--- prac.py
+++ prac.py
@@ -866,19 +866,19 @@
 
 ##write a program insertion sort to sort a list of numbers in ascending order.
 
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
+# def insertion_sort(arr):
+#     for i in range(1, len(arr)):
+#         key = arr[i]
+#         j = i - 1
 
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
+#         while j >= 0 and key < arr[j]:
+#             arr[j + 1] = arr[j]
+#             j -= 1
+#         arr[j + 1] = key
 
-    return arr
+#     return arr
 
 # Example usage:
-numbers = [12, 11, 13, 5, 6]
-sorted_numbers = insertion_sort(numbers)
-print("Sorted list:", sorted_numbers)  # Output: [5, 6, 11, 12, 13]
+# numbers = [12, 11, 13, 5, 6]
+# sorted_numbers = insertion_sort(numbers)
+# print("Sorted list:", sorted_numbers)  # Output: [5, 6, 11, 12, 13]

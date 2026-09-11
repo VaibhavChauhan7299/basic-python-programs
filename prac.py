ca6399919c43bbf481aca968071315d1f3c18288
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-09-11 11:40:00 +0530

    Practice Program

--- prac.py
+++ prac.py
@@ -885,17 +885,17 @@
 
 ##write a program to selection sort to sort a list of numbers in ascending order.
 
-def selection_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        min_index = i
-        for j in range(i + 1, n):
-            if arr[j] < arr[min_index]:
-                min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-    return arr
+# def selection_sort(arr):
+#     n = len(arr)
+#     for i in range(n):
+#         min_index = i
+#         for j in range(i + 1, n):
+#             if arr[j] < arr[min_index]:
+#                 min_index = j
+#         arr[i], arr[min_index] = arr[min_index], arr[i]
+#     return arr
 
 # Example usage:
-numbers = [64, 25, 12, 22, 11]
-sorted_numbers = selection_sort(numbers)
-print("Sorted list:", sorted_numbers)  # Output: [11, 12, 22, 25, 64]
+# numbers = [64, 25, 12, 22, 11]
+# sorted_numbers = selection_sort(numbers)
+# print("Sorted list:", sorted_numbers)  # Output: [11, 12, 22, 25, 64]

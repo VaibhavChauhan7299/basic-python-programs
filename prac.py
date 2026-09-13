dcfeeae04086e3a802821dbca13cf9bba0fd85bd
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-09-13 21:50:00 +0530

    Practice Program

--- prac.py
+++ prac.py
@@ -916,3 +916,41 @@
 # numbers = [3, 6, 8, 10, 1, 2, 1]
 # sorted_numbers = quick_sort(numbers)
 # print("Sorted list:", sorted_numbers)  # Output: [1, 1, 2, 3, 6, 8, 10]     
+
+## write a program to heap sort to sort a list of numbers in ascending order.
+
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+    return arr
+
+# Example usage:
+numbers = [12, 11, 13, 5, 6, 7]
+sorted_numbers = heap_sort(numbers)
+print("Sorted list:", sorted_numbers)  # Output: [5, 6, 7, 11, 12, 13]
+
+
+                                

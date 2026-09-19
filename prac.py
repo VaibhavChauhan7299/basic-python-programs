e5c6bc85126d24b426d805998e045679d2893432
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-09-20 05:15:00 +0530

    Practice Program

--- prac.py
+++ prac.py
@@ -1078,13 +1078,13 @@
 
 ## Find common elements from both lists without duplicates.
 
-list1 = [1, 2, 3, 4, 5]
-list2 = [4, 5, 6, 7, 8]
+# list1 = [1, 2, 3, 4, 5]
+# list2 = [4, 5, 6, 7, 8]
 
-result = []
+# result = []
 
-for num in list1:
-    if num in list2 and num not in result:
-        result.append(num)
+# for num in list1:
+#     if num in list2 and num not in result:
+#         result.append(num)
 
-print(result)  # Output: [4, 5]        
+# print(result)  # Output: [4, 5]        
